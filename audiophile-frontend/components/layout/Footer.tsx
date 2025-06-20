80c43779c900b60afee4b820932d87560cb5682d
snr-roko <rbbagyei@outlook.com>
2025-06-20 08:46:26 +0000

    feat: update Header and Footer components with navigation links and social media icons

--- audiophile-frontend/components/layout/Footer.tsx
+++ audiophile-frontend/components/layout/Footer.tsx
@@ -1,6 +1,31 @@
+import Link from "next/link"
+import logo from "@/public/assets/shared/desktop/logo.svg"
+import Image from "next/image"
+import facebook from "@/public/assets/shared/desktop/icon-facebook.svg"
+import twitter from "@/public/assets/shared/desktop/icon-twitter.svg"
+import instagram from "@/public/assets/shared/desktop/icon-instagram.svg"
+
 const Footer: React.FC = () => {
   return (
-    <footer></footer>
+    <footer className="bg-[#101010] text-[#ffffff] px-4 md:px-30">
+      <div className="flex items-center flex-col gap-10 py-10">
+        <Image src={logo} width={130} height={130} alt="logo" />
+        <nav className="flex gap-3 flex-col justify-center items-center">
+          <Link href="#">HOME</Link>
+          <Link href="#">HEADPHONES</Link>
+          <Link href="#">SPEAKERS</Link>
+          <Link href="#">EARPHONES</Link>
+        </nav>
+        <p className="text-center text-[#ffffff]/70">Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of your personal audio. Come and visit our demo facility - we're open 7 days a week.</p>
+        <p className="text-[#ffffff]/70">Copyright 2021. All Rights Reserved</p>
+        <div className="flex justify-center gap-4">
+          <Image src={facebook} alt="facebook" height={20} width={20}/>
+          <Image src={twitter} alt="twitter" height={20} width={20}/>
+          <Image src={instagram} alt="instagram" height={20} width={20}/>
+        </div>        
+      </div>
+
+    </footer>
   )
 }
 
--- audiophile-frontend/components/layout/Header.tsx
+++ audiophile-frontend/components/layout/Header.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link"
-import { Menu, ShoppingCart } from "lucide-react"
+import logo from "@/public/assets/shared/desktop/logo.svg"
+import shoppingCart from "@/public/assets/shared/desktop/icon-cart.svg"
+import menu from "@/public/assets/shared/tablet/icon-hamburger.svg"
+import Image from "next/image"
 
 const Header: React.FC = () => {
   return (
     <header className="bg-[#101010] text-[#ffffff] px-4 md:px-30">
       <div className="flex justify-between items-center border-[#ffffff] border-b-1 py-8">
-        <Menu className="md:hidden"/>
-        <h1 className="text-3xl font-bold">audiophile</h1>
+        <Image src={menu} alt="menu" height={20} width={20} className="md:hidden"/>
+        <Image src={logo} width={130} height={130} alt="logo"/>
         <div className="hidden md:flex gap-10">
           <Link className="hover:text-[#FBAF85]" href="#">HOME</Link>
           <Link className="hover:text-[#FBAF85]" href="#">HEADPHONES</Link>
           <Link className="hover:text-[#FBAF85]" href="#">SPEAKERS</Link>
           <Link className="hover:text-[#FBAF85]" href="#">EARPHONES</Link>
         </div>
-        <ShoppingCart />
+        <Image src={shoppingCart} alt="shopping cart" height={20} width={20} />
       </div>
     </header>
   )
