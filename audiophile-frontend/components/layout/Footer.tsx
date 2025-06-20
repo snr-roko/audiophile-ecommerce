1acaeec0b35c66d11d57cfd874324a2c6a4e7092
snr-roko <rbbagyei@outlook.com>
2025-06-20 09:41:47 +0000

    feat: enhance Header and Footer components with improved layout and styling

--- audiophile-frontend/components/layout/Footer.tsx
+++ audiophile-frontend/components/layout/Footer.tsx
@@ -7,21 +7,23 @@ import instagram from "@/public/assets/shared/desktop/icon-instagram.svg"
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-[#101010] text-[#ffffff] px-4 md:px-30">
-      <div className="flex items-center flex-col gap-10 py-10">
-        <Image src={logo} width={130} height={130} alt="logo" />
-        <nav className="flex gap-3 flex-col justify-center items-center">
-          <Link href="#">HOME</Link>
-          <Link href="#">HEADPHONES</Link>
-          <Link href="#">SPEAKERS</Link>
-          <Link href="#">EARPHONES</Link>
+    <footer className="bg-[#101010] text-[#ffffff] px-7 lg:px-30">
+      <div className="flex items-center md:items-start flex-col gap-10 py-10">
+        <Image src={logo} width={140} height={140} alt="logo" />
+        <nav className="flex gap-3 flex-col justify-center items-center md:items-start md:flex-row">
+          <Link className="hover:text-[#FBAF85]" href="#">HOME</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">HEADPHONES</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">SPEAKERS</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">EARPHONES</Link>
         </nav>
-        <p className="text-center text-[#ffffff]/70">Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of your personal audio. Come and visit our demo facility - we're open 7 days a week.</p>
-        <p className="text-[#ffffff]/70">Copyright 2021. All Rights Reserved</p>
-        <div className="flex justify-center gap-4">
-          <Image src={facebook} alt="facebook" height={20} width={20}/>
-          <Image src={twitter} alt="twitter" height={20} width={20}/>
-          <Image src={instagram} alt="instagram" height={20} width={20}/>
+        <p className="text-center text-[#ffffff]/70 md:text-left">Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of your personal audio. Come and visit our demo facility - we're open 7 days a week.</p>
+        <div className="flex flex-col gap-10 md:flex-row md:justify-between w-full md:mt-8">
+          <p className="text-[#ffffff]/70 text-center md:text-left">Copyright 2021. All Rights Reserved</p>
+          <div className="flex justify-center gap-4">
+            <Image src={facebook} alt="facebook" height={20} width={25}/>
+            <Image src={twitter} alt="twitter" height={20} width={25}/>
+            <Image src={instagram} alt="instagram" height={20} width={25}/>
+          </div>
         </div>        
       </div>
 
--- audiophile-frontend/components/layout/Header.tsx
+++ audiophile-frontend/components/layout/Header.tsx
@@ -6,11 +6,11 @@ import Image from "next/image"
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-[#101010] text-[#ffffff] px-4 md:px-30">
+    <header className="bg-[#101010] text-[#ffffff] px-7 lg:px-30">
       <div className="flex justify-between items-center border-[#ffffff] border-b-1 py-8">
-        <Image src={menu} alt="menu" height={20} width={20} className="md:hidden"/>
-        <Image src={logo} width={130} height={130} alt="logo"/>
-        <div className="hidden md:flex gap-10">
+        <Image src={menu} alt="menu" height={20} width={20} className="lg:hidden"/>
+        <Image src={logo} width={140} height={140} alt="logo"/>
+        <div className="hidden lg:flex gap-10">
           <Link className="hover:text-[#FBAF85]" href="#">HOME</Link>
           <Link className="hover:text-[#FBAF85]" href="#">HEADPHONES</Link>
           <Link className="hover:text-[#FBAF85]" href="#">SPEAKERS</Link>
