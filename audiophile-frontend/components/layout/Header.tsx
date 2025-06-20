e4e9b943235a7889d8ca7e96eb9c3c8c59029de2
snr-roko <rbbagyei@outlook.com>
2025-06-20 10:53:41 +0000

    feat: update Home page with hero images and product details

--- audiophile-frontend/components/layout/Header.tsx
+++ audiophile-frontend/components/layout/Header.tsx
@@ -6,8 +6,8 @@ import Image from "next/image"
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-[#101010] text-[#ffffff] px-7 lg:px-30">
-      <div className="flex justify-between items-center border-[#ffffff] border-b-1 py-8">
+    <header className="bg-[#191919] text-[#ffffff] px-7 lg:px-30">
+      <div className="flex justify-between items-center border-[#ffffff]/40 border-b-1 py-8">
         <Image src={menu} alt="menu" height={20} width={20} className="lg:hidden"/>
         <Image src={logo} width={140} height={140} alt="logo"/>
         <div className="hidden lg:flex gap-10">
--- audiophile-frontend/pages/index.tsx
+++ audiophile-frontend/pages/index.tsx
@@ -1,6 +1,43 @@
+import Image from "next/image"
+import HeroDesktop from "@/public/assets/home/desktop/image-hero.jpg"
+import HeroTablet from "@/public/assets/home/tablet/image-header.jpg"
+import HeroMobile from "@/public/assets/home/mobile/image-header.jpg"
+
 const Home: React.FC = () => {
   return (
-    <div></div>
+    <div className="relative">
+      <Image
+      src={HeroMobile}
+      className="block md:hidden object-cover w-full h-auto"
+      alt="Headphone Banner"
+      priority
+      />
+      <Image
+      src={HeroTablet}
+      className="hidden md:block lg:hidden object-cover w-full h-auto"
+      alt="Headphone Banner"
+      priority
+      />
+      <Image
+      src={HeroDesktop}
+      className="hidden lg:block object-cover w-full h-auto"
+      alt="Headphone Banner"
+      priority
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+        <h2 className="text-white text-xl md:text-2xl lg:text-3xl tracking-widest mb-4">
+          NEW PRODUCT
+        </h2>
+        <div className="text-white text-xl md:text-2xl lg:text-3xl font-[900] tracking-widest mb-4 text-center">
+          <h1>XX99 MARK II</h1>
+          <h1>HEADPHONES</h1>
+        </div>
+        <p className="text-white text-xl md:text-2xl lg:text-3xl tracking-widest mb-4 text-center">
+          Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+
+        </p>
+      </div>
+    </div>
   )
 }
 
