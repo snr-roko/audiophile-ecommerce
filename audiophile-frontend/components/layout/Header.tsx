8f77bdee91529fe7c6f59e62b596db949b12664b
snr-roko <rbbagyei@outlook.com>
2025-06-20 08:00:37 +0000

    feat: enhance Header layout and add Manrope font with new styles

--- audiophile-frontend/components/layout/Header.tsx
+++ audiophile-frontend/components/layout/Header.tsx
@@ -1,6 +1,21 @@
+import Link from "next/link"
+import { Menu, ShoppingCart } from "lucide-react"
+
 const Header: React.FC = () => {
   return (
-    <header></header>
+    <header className="bg-[#101010] text-[#ffffff] px-4 md:px-30">
+      <div className="flex justify-between items-center border-[#ffffff] border-b-1 py-8">
+        <Menu className="md:hidden"/>
+        <h1 className="text-3xl font-bold">audiophile</h1>
+        <div className="hidden md:flex gap-10">
+          <Link className="hover:text-[#FBAF85]" href="#">HOME</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">HEADPHONES</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">SPEAKERS</Link>
+          <Link className="hover:text-[#FBAF85]" href="#">EARPHONES</Link>
+        </div>
+        <ShoppingCart />
+      </div>
+    </header>
   )
 }
 
--- audiophile-frontend/components/layout/Layout.tsx
+++ audiophile-frontend/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { ReactComponentProps } from "@/interfaces"
 
 const Layout: React.FC<ReactComponentProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mx-auto font-[Manrope]">
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
